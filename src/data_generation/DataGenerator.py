8ed6806a78ef731367f01aa4143ddcee8cbafb00
Bert Ramlot <bert.ramlot@supportsquare.io>
2023-12-11 16:26:24 +0100

    Merged demo and ball overlay into one

--- src/data_generation/DataGenerator.py
+++ src/data_generation/DataGenerator.py
@@ -1,40 +1,30 @@
-import os
 import cv2
-import ctypes
+import time
 import random
 import csv
-import cv2
+import numpy as np
 from pathlib import Path
 
 from src.FaceDetector import FaceDetector
 
 class DataGenerator():
-    def __init__(self, data_set_path: Path, buffer_size: int):
-        self.images_dir_path = data_set_path / "raw"
-        self.meta_data_path = data_set_path / "meta_data.csv"
-        os.makedirs(self.images_dir_path, exist_ok=True)
+    def __init__(self, dataset_path: Path, buffer_size: int):
+        self.images_dir_path = dataset_path / "raw"
+        self.images_dir_path.mkdir(parents=True, exist_ok=True)
+        self.meta_data_path = dataset_path / "meta_data.csv"
 
+        self.start_time = time.time()
+        self.last_register_time = 0
         self.buffer_size = buffer_size
         self._buffer = []
 
-        self.screen_dims = [ctypes.windll.user32.GetSystemMetrics(i) for i in range(2)]
-
-        print("Starting camera ... (this can take a while, I don't know why)")
-        self.cap = cv2.VideoCapture(0)
-        self.face_detector = FaceDetector(self.cap)
-    
-    def exit(self):
-        self.cap.release()
-
     def flush(self) -> None:
-        while self.buffer_length() > 0:
+        while len(self._buffer) > 0:
             self.save_oldest_buffer_item()
 
     def clear_buffer(self) -> None:
         self._buffer = []
 
-    def buffer_length(self) -> int:
-        return len(self._buffer)
 
     @staticmethod
     def generate_filename(parent_path: Path, x: int, y: int):
@@ -70,28 +60,53 @@ class DataGenerator():
         meta_data += [round(rx, prec) for rx in face.features_rx]
         meta_data += [round(ry, prec) for ry in face.features_ry]
 
-        headers = ['testing', 'face_file_name', 'x_screen', 'y_screen', 'tl_rx', 'tl_ry', 'rw', 'rh']
-        headers += ['fx_{}'.format(i) for i in range(len(face.features_rx))]
-        headers += ['fy_{}'.format(i) for i in range(len(face.features_ry))]
-        
         file_exists = self.meta_data_path.is_file()
-        with open(self.meta_data_path, "a+", newline='',encoding="UTF8") as f:
+        with open(self.meta_data_path, "a+", newline="", encoding="UTF8") as f:
             writer = csv.writer(f)
             if not file_exists:
+                headers  = ["testing", "face_file_name", "x_screen", "y_screen", "tl_rx", "tl_ry", "rw", "rh"]
+                headers += [f"fx_{i}" for i in range(len(face.features_rx))]
+                headers += [f"fy_{i}" for i in range(len(face.features_ry))]
                 writer.writerow(headers)
             writer.writerow(meta_data)
 
-    # return true if valid eyepair(s) found
-    def register_eye_position(self, x: int, y: int) -> bool:
-        self.face_detector.update()
+    def register_sample(self, face, x: int, y: int):
+        self._buffer.append((face, x, y))
 
-        if not self.face_detector.valid_faces_found():
-            return False
-
-        for face in self.face_detector.last_faces:
-            self._buffer.append((face, x, y))
-
-        if self.buffer_length() >= self.buffer_size:
+        if len(self._buffer) >= self.buffer_size:
             self.save_oldest_buffer_item()
+
+        self.last_register_time = time.time()
+
+    def get_target_position(self):
+        raise NotImplementedError()
+
+class BallDataGenerator(DataGenerator):
+    def __init__(self, dataset_path: Path, buffer_size: int):
+        DataGenerator.__init__(self, dataset_path, buffer_size)
+        self.ball_pos = np.array([0.5, 0.5], dtype=np.float32)
+        self.ball_vel = np.array([0.15, 0.15], dtype=np.float32)
         
-        return True
+        self.last_update_time = None
+
+    # Override        
+    def get_target_position(self):
+        if self.last_update_time is None:
+            self.last_update_time = time.time()
+        dt = time.time() - self.last_update_time
+        if dt > 0.01:
+            # Update ball position
+            self.ball_pos += self.ball_vel*dt
+            out_of_bounds_mask = (self.ball_pos < 0) | (self.ball_pos > 1)
+            self.ball_vel *= np.where(out_of_bounds_mask, -1, 1)
+            self.ball_pos = np.clip(self.ball_pos, 0, 1)
+            if out_of_bounds_mask.any():
+                rnd_angle = random.random()*np.pi/2
+                vel_mag = np.linalg.norm(self.ball_vel)
+                self.ball_vel = np.copysign(vel_mag * np.array([np.cos(rnd_angle), np.sin(rnd_angle)]), self.ball_vel)
+
+            self.last_update_time = time.time()
+
+        time_since_start = time.time() - self.start_time
+        time_since_register = time.time() - self.last_register_time
+        return (self.ball_pos, time_since_register > 0.5 and time_since_start > 5)
--- src/demo.py
+++ src/demo.py
@@ -1,100 +1,49 @@
-import ctypes
 import sys
 from pathlib import Path
 from argparse import ArgumentParser
-from PyQt5 import QtGui, QtWidgets, QtCore
-import cv2
 import torch
-import numpy as np
+from PyQt5 import QtWidgets
 
-from src.FaceNeuralNetwork import FaceDataset, FaceNeuralNetwork
-from src.FaceDetector import FaceDetector
+from src.EyeTrackingOverlay import EyeTrackingOverlay
 
-
-class EyeTrackingOverlay(QtWidgets.QMainWindow):
-    def __init__(self, model, history_amount: int, device):
-        QtWidgets.QMainWindow.__init__(self, None, QtCore.Qt.WindowStaysOnTopHint)
-
-        self.screen_dims = np.array([ctypes.windll.user32.GetSystemMetrics(i) for i in range(2)])
-        self.setGeometry(0, 0, *self.screen_dims)
-        self.setStyleSheet("background:transparent")
-        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
-        self.setAttribute(QtCore.Qt.WA_TranslucentBackground, True)
-
-        self.device = device
-        self.history_amount = history_amount
-        self.pred_locations_history = [[] for _ in range(history_amount)]
-
-        self.model = model
-
-        print("Starting camera ...")
-        self.cap = cv2.VideoCapture(0)
-        self.faceDetector = FaceDetector(self.cap)
-
-        timer = QtCore.QTimer(self, timeout=self.update_eyes)
-        timer.start()
-
-    #region Painting
-    def paintEvent(self, e):
-        qp = QtGui.QPainter()
-        qp.begin(self)
-        self.drawPredictedLocations(qp)
-        qp.end()
-
-    def drawPredictedLocations(self, qp):
-        colors = [QtCore.Qt.blue, QtCore.Qt.green, QtCore.Qt.red, QtCore.Qt.yellow]
-
-        pen = QtGui.QPen(QtCore.Qt.green, 4, QtCore.Qt.SolidLine)
-
-        if len(self.pred_locations_history[-1]) == 0:
-            r = 200
-            x, y = self.screen_dims // 2
-            qp.setPen(pen)
-            qp.drawEllipse(x-r, y-r, 2*r, 2*r)
-            qp.drawText(x-50, y, "No valid face(s) found")
-        else:
-            r = 30
-            for i, pred_loc in enumerate(self.pred_locations_history[-1]):
-                pen.setColor(colors[i % len(colors)])
-                qp.setPen(pen)
-                qp.drawEllipse(pred_loc[0]-r, pred_loc[1]-r, 2*r, 2*r)
-    #endregion
-
-    def exit(self):
-        self.cap.release()
-
-    @QtCore.pyqtSlot()
-    def update_eyes(self):
-        self.faceDetector.update()
-
-        pred_locations = []
-        if self.faceDetector.valid_faces_found():
-            faces_input = torch.stack([FaceDataset.face_to_tensor(face, self.device) for face in self.faceDetector.last_faces])
-            predictions = self.model(faces_input).cpu().detach().numpy()
-            pred_locations = (np.clip(predictions, 0.0, 1.0) * self.screen_dims).round().astype(np.int32)
-        self.pred_locations_history = self.pred_locations_history[1:]
-        self.pred_locations_history.append(pred_locations)
-
-        self.update()
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Demo script parameters")
-    parser.add_argument("--model_name", type=str, required=True)
+    parser.add_argument("--model", type=str, default=None)
     parser.add_argument('--device', type=str, default="cuda")
     parser.add_argument("--epoch", type=int, default=None)
+    parser.add_argument("--dataset", type=str, default=None)
     args = parser.parse_args(sys.argv[1:])
 
-    model_path = Path("models") / args.model_name
-    if args.epoch is None:
-        pth_path = max(model_path.glob("epoch_*.pth"), default=None)
+    if args.model:
+        print("Model passed, doing inference")
+        from src.FaceNeuralNetwork import FaceNeuralNetwork
+
+        model_path = Path("models") / args.model
+        if args.epoch is None:
+            pth_path = max(model_path.glob("epoch_*.pth"), default=None)
+        else:
+            pth_path = model_path / f"epoch_{args.epoch}.pth"
+        if pth_path is None or not pth_path.exists():
+            raise FileNotFoundError("Failed to find model specified")
+        
+        checkpoint = torch.load(pth_path)
+        model = FaceNeuralNetwork().to(args.device)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        model.eval()
     else:
-        pth_path = model_path / f"epoch_{args.epoch}.pth"
-    checkpoint = torch.load(pth_path)
-    model = FaceNeuralNetwork().to(args.device)
-    model.load_state_dict(checkpoint['model_state_dict'])
-    model.eval()
+        model = None
+
+    if args.dataset:
+        print("Dataset passed, generating data")
+        from src.data_generation.DataGenerator import BallDataGenerator
+
+        data_generator = BallDataGenerator(Path("datasets") / args.dataset, 200)
+    else:
+        data_generator = None
+
 
     app = QtWidgets.QApplication(sys.argv)
-    window = EyeTrackingOverlay(model, 10, args.device)
+    window = EyeTrackingOverlay(args.device, model=model, data_generator=data_generator)
     window.showFullScreen()
     sys.exit(app.exec_())
