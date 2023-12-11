d427057cff81a20121ddae92bb18fb873fb41065
Bert Ramlot <bert.ramlot@supportsquare.io>
2023-12-11 16:27:01 +0100

    added support for arbitrairy amount of loss functions (for logging)

--- src/train.py
+++ src/train.py
@@ -1,5 +1,5 @@
-import os
 import sys
+import numpy as np
 from pathlib import Path
 from argparse import ArgumentParser
 import torch
@@ -16,56 +16,73 @@ except ImportError:
 from src.FaceNeuralNetwork import FaceNeuralNetwork, FaceDataset
 
 
-def test_epoch(test_loader, model, criterion, device):
-    epoch_loss = 0
+def test_epoch(test_loader, model, device, loss_functions):
+    losses = { name:0 for name in loss_functions}
     with torch.no_grad():
         for X, y in test_loader:
             X = X.to(device, non_blocking=True)
             y = y.to(device, non_blocking=True)
 
             output = model(X)
-            loss = criterion(output, y)
-            epoch_loss += loss.item()
-    
-    return epoch_loss / len(test_loader)
+            for name, loss_fn in loss_functions.items():
+                loss = loss_fn(output, y)
+                losses[name] += loss.item()
+   
+    for name in losses:
+        losses[name] /= len(test_loader)
+        
+    return losses
 
-def train_one_epoch(train_loader, model, criterion, optimizer, device):
-    epoch_loss = 0
-    for batch, (X, y) in enumerate(train_loader):
+def train_one_epoch(train_loader, model, device, loss_functions, optimizer):
+    if "criterion" not in loss_functions:
+        raise AttributeError("No criterion found")
+    
+    losses = { name:0.0 for name in loss_functions}
+    for X, y in train_loader:
         X = X.to(device, non_blocking=True)
         y = y.to(device, non_blocking=True)
 
         output = model(X)
-        loss = criterion(output, y)
-        epoch_loss += loss.item()
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        for name, loss_fn in loss_functions.items():
+            loss = loss_fn(output, y)
+            losses[name] += loss.item()
+            if name == "criterion":
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
     
-    return epoch_loss / len(train_data_loader)
+    for name in losses:
+        losses[name] /= len(train_loader)
 
-def training_report(tb_writer, epoch, train_loss, test_loss):
-    if tb_writer:
-        tb_writer.add_scalar('loss/train_l2', train_loss, epoch)
-        tb_writer.add_scalar('loss/test_l2', test_loss, epoch)
+    return losses 
 
-    print(f"{epoch} | train_loss={train_loss}, test_loss={test_loss}")
+def training_report(tb_writer, epoch, train_losses, test_losses):
+    for name in train_losses.keys() & test_losses.keys():
+        dict = {}
+        if name in train_losses:
+            dict["train"] = train_losses[name]
+        if name in test_losses:
+            dict["test"] = test_losses[name]
+        if tb_writer:
+            tb_writer.add_scalars(f'{name}_loss', dict, epoch)
+
+    print(f"{epoch:6} | train_loss={train_losses['criterion']:.5f}, test_loss{test_losses['criterion']:.5f}")
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Training script parameters")
     parser.add_argument("--model_name", type=str, required=True)
-    parser.add_argument("--data_set_name", type=str, required=True)
+    parser.add_argument("--dataset", type=str, required=True)
     parser.add_argument('--device', type=str, default="cuda")
     parser.add_argument("--max_epochs", type=int, default=None)
     parser.add_argument("--lr", type=float, default=1e-2)
     args = parser.parse_args(sys.argv[1:])
 
     model_path = Path("models") / args.model_name
-    data_set_path = Path("data_sets") / args.data_set_name
-    os.makedirs(model_path, exist_ok = True)
-    os.makedirs(data_set_path, exist_ok = True)
+    model_path.mkdir(parents=True, exist_ok=True)
+    dataset_path = Path("datasets") / args.dataset
+    if not dataset_path.exists():
+        raise FileNotFoundError("dataset not found")
 
     tb_writer = SummaryWriter(model_path) if TENSORBOARD_FOUND else None
 
@@ -73,19 +90,19 @@ if __name__ == "__main__":
     loss_fn = torch.nn.MSELoss()
 
     print("Loading training data set ... ", end="")
-    train_data_set = FaceDataset(data_set_path, args.device, testing=False)
-    train_data_loader = DataLoader(train_data_set, batch_size=32, shuffle=True)
-    print(f"{len(train_data_set)} samples loaded")
+    train_dataset = FaceDataset(dataset_path, args.device, testing=False)
+    train_data_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    print(f"{len(train_dataset)} samples loaded")
 
     print("Loading testing data set ... ", end="")
-    test_data_set = FaceDataset(data_set_path, args.device, testing=True)
-    test_data_loader = DataLoader(test_data_set, batch_size=1)
-    print(f"{len(test_data_set)} samples loaded")
+    test_dataset = FaceDataset(dataset_path, args.device, testing=True)
+    test_data_loader = DataLoader(test_dataset, batch_size=1)
+    print(f"{len(test_dataset)} samples loaded")
 
     model = FaceNeuralNetwork().to(args.device)
     model.train()
     # optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, eps=1e-15)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
     last_epoch_file = max(model_path.glob("epoch_*.pth"), default=None)
     if last_epoch_file is None:
@@ -99,20 +116,19 @@ if __name__ == "__main__":
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
     print("Starting training")
-    def criterion(output, target):
-        clamped = torch.clamp(output, min=0.0, max=1.0)
-        return loss_fn(clamped, target)
-
+    loss_functions = {
+        "L1": torch.nn.L1Loss(),
+        "L2": torch.nn.MSELoss(),
+        "criterion": torch.nn.MSELoss()
+    }
     max_epochs = 1e7 if args.max_epochs is None else args.max_epochs
     while epoch < max_epochs:
-        train_loss = train_one_epoch(train_data_loader, model, criterion, optimizer, args.device)
-
-        # model.eval()
-        test_loss = test_epoch(test_data_loader, model, criterion, args.device)
+        train_losses = train_one_epoch(train_data_loader, model, args.device, loss_functions, optimizer)
+        test_losses = test_epoch(test_data_loader, model, args.device, loss_functions)
         
-        training_report(tb_writer, epoch, train_loss, test_loss)
+        training_report(tb_writer, epoch, train_losses, test_losses)
 
-        if epoch % 100 == 0:
+        if epoch % 100 == 0 or epoch+1 == max_epochs:
             torch.save(
                 {
                     'epoch': epoch,
