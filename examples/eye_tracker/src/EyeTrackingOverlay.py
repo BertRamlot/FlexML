2a7b4c8eac61acb8faa1be1f5b9ddbacfa64b737
Bert Ramlot <bert.ramlot@supportsquare.io>
2024-02-12 12:54:57 +0100

    cleanup

--- examples/eye_tracker/src/EyeTrackingOverlay.py
+++ examples/eye_tracker/src/EyeTrackingOverlay.py
@@ -1,4 +1,3 @@
-import logging
 import numpy as np
 import time
 from PyQt6 import QtGui, QtWidgets, QtCore
@@ -7,46 +6,57 @@ from examples.eye_tracker.src.FaceSample import FaceSample
 
 
 class EyeTrackingOverlay(QtWidgets.QMainWindow):
-
     # TODO: hard coded
     GROUND_TRUTH_HISTORY_LENGTH = 20
-    INFERENCE_HISTORY_LENGTH = 50
-    INFERENCE_HISTORY_TIME = 1.0
+    PREDICTION_HISTORY_TIME_WINDOW = 1.0
 
     def __init__(self, window_dims: np.ndarray):
+        """
+        Initialize the EyeTrackingOverlay.
+
+        Args:
+            window_dims (np.ndarray): Dimensions of the overlay window, shape is (2,) with format (height, width).
+        """
         super().__init__(flags=QtCore.Qt.WindowType.FramelessWindowHint | QtCore.Qt.WindowType.WindowStaysOnTopHint)
         self.window_dims = window_dims # [h, w]
         self.ground_truth_history: list[np.ndarray] = []
-        self.inference_history: dict[int, tuple[list[float, np.ndarray]]] = {}
+        self.prediction_history: dict[int, list[tuple[float, np.ndarray]]] = {}
 
-        self.setGeometry(0, 0, self.window_dims[1], self.window_dims[0])
+        self.setGeometry(0, 0, *np.flip(self.window_dims))
         self.setStyleSheet("background:transparent")
         self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
         self.showFullScreen()
 
     @QtCore.pyqtSlot(np.ndarray)
-    def register_gt_position(self, pos: np.ndarray):
-        self.ground_truth_history = self.ground_truth_history[1-EyeTrackingOverlay.GROUND_TRUTH_HISTORY_LENGTH:] + [pos]
+    def register_ground_truth(self, pos: np.ndarray):
+        """
+        Register the ground truth position.
+
+        Args:
+            pos (np.ndarray): Ground truth position, shape is (2,) with format (y, x).
+        """
+        self.ground_truth_history = self.ground_truth_history[-(EyeTrackingOverlay.GROUND_TRUTH_HISTORY_LENGTH-1):] + [pos]
         self.update()
 
     @QtCore.pyqtSlot(list, np.ndarray)
-    def register_inference_positions(self, face_samples: list[FaceSample], predictions: np.ndarray):
+    def register_predictions(self, face_samples: list[FaceSample], predictions: np.ndarray):
+        """
+        Register predictions for a list of face samples.
+
+        Args:
+            face_samples (list[FaceSample]): List of N face samples.
+            predictions (np.ndarray): Prediction per face sample, shape is (N, 2) where each row is a (y, x) position.
+        """
         now_time = time.time()
         for face_sample, prediction in zip(face_samples, predictions):
-            if face_sample.face_id in self.inference_history:
-                min_time = now_time - EyeTrackingOverlay.INFERENCE_HISTORY_TIME
-                new_history = [(time, pos) for time, pos in self.inference_history[face_sample.face_id] if time >= min_time]
+            if face_sample.face_id in self.prediction_history:
+                min_time = now_time - EyeTrackingOverlay.PREDICTION_HISTORY_TIME_WINDOW
+                new_history = [(time, pos) for time, pos in self.prediction_history[face_sample.face_id] if time >= min_time]
             else: 
                 new_history = []
             new_history.append((now_time, prediction))
-            self.inference_history[face_sample.face_id] = new_history
+            self.prediction_history[face_sample.face_id] = new_history
         self.update()
-
-    def keyPressEvent(self, event: QtGui.QKeyEvent) -> None:
-        super().keyPressEvent(event)
-        if event.key() == ord("q"):
-            logging.info("Closing overlay")
-            self.close()
 
     def paintEvent(self, e):
         qp = QtGui.QPainter()
@@ -59,7 +69,7 @@ class EyeTrackingOverlay(QtWidgets.QMainWindow):
         if not self.ground_truth_history:
             return
         
-        # Draws the path of the ground_truth
+        # Draws the path of the ground truth
         # polygon = QtGui.QPolygon()
         # for point in self.ground_truth_history:
         #     polygon.append(QtCore.QPoint(*np.flip(point)))
@@ -86,17 +96,16 @@ class EyeTrackingOverlay(QtWidgets.QMainWindow):
         pen = QtGui.QPen(QtCore.Qt.GlobalColor.green, 4, QtCore.Qt.PenStyle.SolidLine)
 
         now_time = time.time()
-        for face_id in self.inference_history:
-            if len(self.inference_history[face_id]) == 0:
+        for face_id in self.prediction_history:
+            if len(self.prediction_history[face_id]) == 0:
                 continue
-            times, positions = zip(*self.inference_history[face_id])
+            times, positions = zip(*self.prediction_history[face_id])
             
-            # Exponential average over the current prediction_history
-            # This reduces the temporal instabilities: higher alpha => higher weight towards fresher samples
-            alpha = 8.0
+            # Exponential average over the current prediction_history to reduce temporal instabilities.
+            # higher alpha => higher weights for newer samples
+            alpha = 10.0
             factors = np.exp((np.array(times) - now_time) * alpha)
             factors /= factors.sum()
-            # print(factors)
             predicted_pos = (factors[:, np.newaxis] * positions).sum(axis=0) 
             # Put out of bound coordinates to the edge of the screen. This is not done in the training loop
             predicted_pos = (np.clip(predicted_pos, 0.0, 1.0) * self.window_dims.max()).round().astype(np.int32)
