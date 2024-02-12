8b464890b71e22cebb08310282dc9e1ed0dc94c9
Bert Ramlot <bert.ramlot@supportsquare.io>
2024-02-12 13:58:56 +0100

    cleanup

--- FlexML/SourceObject.py
+++ FlexML/SourceObject.py
@@ -11,47 +11,41 @@ class SourceObject(QObject):
     new_object = pyqtSignal(object)
 
     def __init__(self, timeout: float, use_seperate_thread: bool):
-        super().__init__(None)
-        
+        super().__init__()
         self.timeout = timeout
-        
-        self._timer = None
         self._thread = None
-
+        self._timer = None
         if use_seperate_thread:
             self._thread = QThread()
             self.moveToThread(self._thread)
             self._thread.started.connect(self._thread_run)
         else:
             self._timer = QTimer()
-            self._timer.timeout.connect(self.process)
+            self._timer.timeout.connect(self._timer_run)
     
     def _thread_run(self):
-        while True:
+        while not self.is_done():
             t0 = time.time()
-            done = self.process()
+            success, item = self.get()
+            if success:
+                self.new_object.emit(item)
             self._thread.eventDispatcher().processEvents(QEventLoop.ProcessEventsFlag.AllEvents)
-            if done:
-                break
             t1 = time.time()
-            sleep_ms = int(1000*(self.timeout - (t1 - t0)))
+            sleep_ms = int(1000 * (self.timeout - (t1 - t0)))
             if sleep_ms > 0:
                 self._thread.msleep(sleep_ms)        
     
-    def process(self) -> bool:
+    def _timer_run(self) -> bool:
         if self.is_done():
-            if self._timer is not None:
-                self._timer.stop()
-            return True
+            self._timer.stop()
 
         success, item = self.get()
         if success:
             self.new_object.emit(item)
-        return False
             
     def start(self):
         if self._timer is not None:
-            timeout_ms = int(1000*(self.timeout))
+            timeout_ms = int(1000 * self.timeout)
             self._timer.start(timeout_ms)
         if self._thread is not None:
             self._thread.start()
@@ -66,7 +60,6 @@ class WebcamSourceObject(SourceObject):
     def __init__(self, timeout: int, index: int = 0):
         super().__init__(timeout, True)
         logging.info(f"Starting webcam capture (index={index}) ...")
-        # TODO: check if the webcam actually works/exists
         self.cap = cv2.VideoCapture(index, cv2.CAP_DSHOW)
         logging.info(f"Finished starting webcam capture (index={index})")
 
@@ -76,16 +69,27 @@ class WebcamSourceObject(SourceObject):
     def get(self) -> tuple[bool, np.ndarray]:
         return self.cap.read()
 
+    def is_done(self) -> bool:
+        # TODO: check if the webcam still works/exists
+        return False
+
 class VideoFileSourceObject(SourceObject):
     def __init__(self, timeout: int, path: Path):
         super().__init__(timeout, True)
         self.cap = cv2.VideoCapture(str(path))
+        self.reached_end = False
 
     def __del__(self):
         self.cap.release()
 
     def get(self) -> tuple[bool, np.ndarray]:
-        return self.cap.read()
+        ret, frame = self.cap.read()
+        if not ret:
+            self.reached_end = True
+        return ret, frame
+    
+    def is_done(self) -> bool:
+        return self.reached_end
 
 class ScreenSourceObject(SourceObject):
     def __init__(self, timeout: int, monitor: dict[str, int] | tuple[int, int, int, int] | None):
